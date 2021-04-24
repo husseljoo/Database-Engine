356e74ae2f048c436ea504cc2af49fa0ac1e115f
Husseljo <husseljoo@gmail.com>
2021-04-24 17:34:21 +0200

    handled reading .config file page entries and and setting Page class instance variable : max, to this value.

--- src/main/java/Page.java
+++ src/main/java/Page.java
@@ -1,19 +1,66 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.net.URL;
+import java.util.Objects;
+import java.util.Properties;
 import java.util.Vector;
 
 public class Page implements Serializable 
 {
-	Vector<Tuple> tuples;
-	int max;
-	public Page(int n)
-	{
-		tuples = new Vector<Tuple>();
-		max = n;
-	}
+
+	int max=this.pageEntries();
+	Vector<Tuple> tuples=new Vector<Tuple>();
+	Page nextPage;
+	
+	public Page(){}
+
 	public void insert(Tuple l)
 	{
 		if(tuples.size()>=max)
 			return;
 		tuples.add(l);
 	}
+	
+	
+	
+//READ DBapp.config file and extract number of entries per page to set the instance variable
+//max of class Page to this value
+	public int pageEntries() {
+		
+		String config="DBApp.config";
+		Properties prop=new Properties();
+		
+		prop = new java.util.Properties();
+		try {
+		   prop.load(this.getClass().getClassLoader().
+		   getResourceAsStream(config));
+		 }
+		catch(Exception e){
+		     e.printStackTrace();
+		 }
+		int entries=Integer.parseInt(prop.getProperty("MaximumRowsCountinPage"));
+		
+		return entries;
+		}
+	
+	
+	public static void main(String[] args) {
+		Page p=new Page();
+//		int n=p.pageEntries();
+		System.out.println(p.max);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
